5d270aba3eb03855b4447971cb021b63b127c22d
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-23 12:48:39 -0500

    Update eslint config, add global definitions for describe and it functions to avoid eslint errors in mocha tests

--- test/sanity-check_test.js
+++ test/sanity-check_test.js
@@ -1,8 +1,10 @@
+/* global define, it, describe */
+
 const expect = require("chai").expect;
 
 // Sanity Check
 describe("Mocha", () => {
-    it("should run our tests using npm", () => {
-        expect(true).to.be.ok;
-    });
+	it("should run our tests using npm", () => {
+		expect(true).to.be.ok;
+	});
 });
